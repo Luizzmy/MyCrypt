fb8c70e2148f9f859b7321bde45cad3ac61ef02a
Rubén Rodríguez <rbn@iMac-de-Ruben.local>
2020-12-04 09:34:02 -0600

    some changes

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useContextData} from '../hooks/context';
+import {getPost} from '../services/post';
+
+import { Layout, Menu, Row, Col, Card, Avatar } from 'antd'
+const { Header, Footer, Sider, Content } = Layout;
 
 function Profile() {
+  // crud
+const {user} =useContextData()
+const [posts,setPost]= useState([])
+
+useEffect(() => {
+  async function getPosts(){
+    const{data}=await getPost()
+    setPost(data)
+  }
+  
+}, [])
+
+
   return (
     <div>
       <h1>profile test</h1>
+      <Layout>
+        <Row gutter={[16, 16]}>
+          <Col xs={24} sm={24} md={12} lg={12} >
+            <Card title="User profile">
+            Lorem ipsum, dolor sit amet consectepe,a officii
+            m excepturi illum natus voluptas voluptatibus. 
+            Incidunt rerum optio numquam distinctio,dsd
+
+            </Card>
+            
+          </Col>
+          <Col xs={24} sm={24} md={12} lg={12} >
+            <Card title="Post">
+
+            </Card>
+            
+          </Col>
+
+        </Row>
+
+      
+
+
+
+
+
+      </Layout>
+
+
     </div>
   );
 }
--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,35 +1,35 @@
-// js file from generator
+// // js file from generator
 
 
-import axios from 'axios'
+// import axios from 'axios'
 
-const baseURL = process.env.NODE_ENV === 'development' ?
-  'http://localhost:3000/auth' :
-  '/auth'
+// const baseURL = process.env.NODE_ENV === 'development' ?
+//   'http://localhost:3000/auth' :
+//   '/auth'
 
-const authService = axios.create({
-  baseURL,
-  withCredentials: true
-})
+// const authService = axios.create({
+//   baseURL,
+//   withCredentials: true
+// })
 
-//habilitar withCredentials:true en el backend
+// //habilitar withCredentials:true en el backend
 
-const MY_SERVICE = {
-  test: async () => {
-    return await service.get('/');
-  },
-  signup: async (user) => {
-    return await SERVICE.post('/signup', user);
-  },
-  login: async (user) => {
-    return await SERVICE.post('/login', user);
-  },
-  logOut: async () => {
-    return await SERVICE.get('/logout');
-  },
-  currentUser: async () => {
-    return await SERVICE.get('/current-user');
-  }
-};
+// const MY_SERVICE = {
+//   test: async () => {
+//     return await service.get('/');
+//   },
+//   signup: async (user) => {
+//     return await SERVICE.post('/signup', user);
+//   },
+//   login: async (user) => {
+//     return await SERVICE.post('/login', user);
+//   },
+//   logOut: async () => {
+//     return await SERVICE.get('/logout');
+//   },
+//   currentUser: async () => {
+//     return await SERVICE.get('/current-user');
+//   }
+// };
 
-export default MY_SERVICE;
+// export default MY_SERVICE;
